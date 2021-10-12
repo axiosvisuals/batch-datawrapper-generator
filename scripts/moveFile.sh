620023db63b9b5dfbffb11728b4470a18d0e62b0
Jared Whalen <jared.m.whalen@gmail.com>
2021-10-12 16:00:15 -0400

    update

--- scripts/moveFile.sh
+++ scripts/moveFile.sh
@@ -1,7 +1,7 @@
 curl  --request PATCH \
       --url https://api.datawrapper.de/v3/charts/$1 \
       --header 'Authorization: Bearer '$3 \
-        --header 'content-type: application/json' \
+      --header 'content-type: application/json' \
       --data \
      '{
         "folderId": '$2'
